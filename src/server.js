1ca25cfde83d53cb58e516bd501a9a12f0d23226
Likitha <likithamn23cs@rnsit.ac.in>
2025-07-26 20:47:46 +0530

    Day 4: MongoDB setup + server configured

--- src/server.js
+++ src/server.js
@@ -1,21 +1,13 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+connectDB();
+
 const app = express();
-
-let initiateAndServe=async()=>{
-  try{
-    app.listen(3000,()=>{
-      console.log("Server is running on port 3000");    
-    })
-  }catch(e){
-    console.log("Error starting server: " + e.message); 
-    process.exit(1);
-  }
-}
-initiateAndServe();
-
-app.get('/api/test', (req, res) => {
-  res.status(200).send('Hello World!');
-}); 
-
 app.use(express.json());
 
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT}`);
+});
